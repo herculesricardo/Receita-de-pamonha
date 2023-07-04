031b31e1b126f873d91f4f57c582f90df7de2471
herculesricardo <135158468+herculesricardo@users.noreply.github.com>
2023-07-03 21:20:55 -0300

    upgrade

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,35 @@
-var counter = 0;
+const decreaseBtn = document.getElementById('decrease-btn');
+const increaseBtn = document.getElementById('increase-btn');
+const counter = document.getElementById('counter');
+const cornAmount = document.getElementById('corn-amount');
+const sugarAmount = document.getElementById('sugar-amount');
+const butterAmount = document.getElementById('butter-amount');
 
-document.getElementById("increase-btn").addEventListener("click", function() {
-  counter++;
-  document.getElementById("counter").textContent = counter;
-});
+// Adicionar evento de clique aos botões
+decreaseBtn.addEventListener('click', decreaseCounter);
+increaseBtn.addEventListener('click', increaseCounter);
 
-document.getElementById("decrease-btn").addEventListener("click", function() {
-  if (counter > 0) {
-    counter--;
-    document.getElementById("counter").textContent = counter;
+// Função para diminuir o contador e atualizar a quantidade de ingredientes
+function decreaseCounter() {
+  let currentCount = parseInt(counter.textContent);
+  if (currentCount > 1) {
+    currentCount--;
+    counter.textContent = currentCount;
+    updateIngredientList(currentCount);
   }
-});
+}
+
+// Função para aumentar o contador e atualizar a quantidade de ingredientes
+function increaseCounter() {
+  let currentCount = parseInt(counter.textContent);
+  currentCount++;
+  counter.textContent = currentCount;
+  updateIngredientList(currentCount);
+}
+
+// Função para atualizar a quantidade de ingredientes na lista
+function updateIngredientList(count) {
+  cornAmount.textContent = count;
+  sugarAmount.textContent = count;
+  butterAmount.textContent = count;
+}
